a49aeac5d12f971ea6a6d75bc8f51307b7b13373
Ryan Marx <ryan.marx@pulsarinformatics.com>
2026-02-17 00:39:17 -0600

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), svelte()],
-  base: '/energy-charts/',
+  base: '/energy-charts-EFIF/',
   resolve: {
     alias: {
       '@': '/src',
