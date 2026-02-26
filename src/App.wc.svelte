fc5c21e45bc76e140ba56185eff9af308870efeb
Ryan Marx <ryan.marx@pulsarinformatics.com>
2026-02-25 20:33:39 -0600

    Do 1 chart at a time

--- src/App.wc.svelte
+++ src/App.wc.svelte
@@ -12,33 +12,113 @@
   import TBR from './lib/tbr/ChartTBR.svelte';
   import CancelledVsFunded from './lib/cancelledVsFunded/CancelledVsFunded.svelte';
   import styles from '@/app.css?inline';
+  import type { Component } from 'svelte';
+
+  type Chart = {
+    page: 'Home' | 'Spending' | 'PR';
+    Component: Component;
+    title: string;
+  };
+
+  const slugs: Record<string, Chart> = {
+    'HP-Staffing-Cuts': {
+      page: 'Home',
+      Component: StaffingCuts,
+      title: 'Proposed staff reductions from last full fiscal year (2024–2026)',
+    },
+    'PEO-Cuts-Over-Time': {
+      page: 'PR',
+      Component: HistoricalStaffingChanges,
+      title: 'Historical changes in DOE staffing levels',
+    },
+    'PEO-Cost-Of-Cuts': {
+      page: 'PR',
+      Component: ResourcesRatio,
+      title: 'And leave remaining staff with historically large workloads',
+    },
+    FedEmployeeSpending: {
+      Component: FedEmployeeSpending,
+      page: 'Spending',
+      title: 'Spending on federal employees only a small part of DOE budget',
+    },
+    'PR-Awards-Vs-Loans': { page: 'PR', Component: AwardsVsLoans, title: 'PR awards vs loans' },
+    'PR-Cancelled-Vs-funded': {
+      page: 'PR',
+      Component: CancelledVsFunded,
+      title: 'Little consistency in cancelled vs. funded technologies',
+    },
+    'SP-Budget-Proposal-Comparisons': {
+      page: 'Spending',
+      Component: BudgetProposalComparisons,
+      title: 'Congress passed DOE spending that supports nuclear and grid efforts',
+    },
+    'SP-Funding': {
+      Component: ChartStaffing,
+      title: 'Staffing stacked bar chart',
+      page: 'Spending',
+    },
+    'HP-DOE-Spending': {
+      page: 'Home',
+      Component: TBR,
+      title: 'In 2025 DOE Spent a Small Fraction of Its Available Funding',
+    },
+    'SP-Cancelled-funds': {
+      page: 'Spending',
+      Component: SPCancelledFunds,
+      title: 'Over 70% of the cancelled award funding does not expire',
+    },
+  };
+
+  let { slug, picker = false }: { slug?: string; picker?: boolean } = $props();
+
+  const displayPicker = $derived(Boolean(picker));
 </script>
 
 <svelte:element this={'style'}>{styles}</svelte:element>
 
-<main>
-  <h2>Proposed staff reductions from last full fiscal year (2024–2026)</h2>
-  <StaffingCuts></StaffingCuts>
-  <h2>TBR</h2>
-  <TBR {chartHeader} />
-  <h2>Proposed DOE job cuts</h2>
-  <p>TKTK</p>
-  <h2>Staffing</h2>
+{#if displayPicker}
+  <div class="select-container">
+    <select bind:value={slug} class="border">
+      <option disabled>Select a chart</option>
+      <optgroup label="Home page">
+        {#each Object.entries(slugs).filter(([_, { page }]) => page === 'Home') as [slug, { title }] (slug)}
+          {@render optionItem(slug, title)}
+        {/each}
+      </optgroup>
+      <optgroup label="Spending page">
+        {#each Object.entries(slugs).filter(([_, { page }]) => page === 'Spending') as [slug, { title }] (slug)}
+          {@render optionItem(slug, title)}
+        {/each}
+      </optgroup>
+      <optgroup label="Other page">
+        {#each Object.entries(slugs).filter(([_, { page }]) => page === 'PR') as [slug, { title }] (slug)}
+          {@render optionItem(slug, title)}
+        {/each}
+      </optgroup>
+    </select>
+  </div>
+{/if}
+
+<main class="flex flex-col gap-3">
+  {#if slug && slugs[slug]}
+    {@const { Component, title } = slugs[slug]}
+    <h2>{title}</h2>
+    <div class="flex items-center gap-2 bg-amber-100 p-2">
+      <span>Usage:</span>
+      <pre><code>&lt;svelte-app-web-component slug="{slug}">&lt;/svelte-app-web-component></code
+        ></pre>
+    </div>
+    <Component {chartHeader} />
+  {/if}
+  <!-- <h2>Staffing</h2>
   <ChartStaffing {chartHeader} />
+  
+  
   <h2>Congress passed DOE spending that supports nuclear and grid efforts</h2>
   <BudgetProposalComparisons></BudgetProposalComparisons>
-  <h2>Over 70% of the cancelled award funding does not expire</h2>
-  <SPCancelledFunds></SPCancelledFunds>
-  <h2>PR awards vs loans</h2>
-  <AwardsVsLoans></AwardsVsLoans>
-  <h2>Little consistency in cancelled vs. funded technologies</h2>
-  <CancelledVsFunded></CancelledVsFunded>
-  <h2>Spending on federal employees only a small part of DOE budget</h2>
-  <FedEmployeeSpending></FedEmployeeSpending>
-  <h2>And leave remaining staff with historically large workloads</h2>
-  <ResourcesRatio></ResourcesRatio>
-  <h2>Historical changes in DOE staffing levels</h2>
-  <HistoricalStaffingChanges></HistoricalStaffingChanges>
+
+
+  -->
 </main>
 
 {#snippet chartHeader({ header = '', description = '', label = '' })}
@@ -52,6 +132,9 @@
     <p class="chart-label mb-2 text-sm font-bold italic">{label}</p>
   {/if}
 {/snippet}
+{#snippet optionItem(slug = '', title = '')}
+  <option value={slug}>{title}</option>
+{/snippet}
 
 <style lang="postcss">
   :is(.chart-header, .chart-description) + .chart-label {
@@ -59,11 +142,18 @@
   }
 
   h2 {
-    margin: 3rem 0;
-    border-top: 1px solid #aaa;
-    padding-top: 3rem;
     text-align: center;
     text-decoration: underline;
     font-size: 2rem;
+  }
+
+  .select-container {
+    padding: 1rem;
+    width: 100%;
+    background-color: #eee;
+    border: 1px solid #aaa;
+    margin-bottom: 2rem;
+    display: flex;
+    justify-content: center;
   }
 </style>
