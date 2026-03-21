917cbd865c0727f89b220dc10ccfd13fac7f537d
Ryan Marx <ryan.marx@pulsarinformatics.com>
2026-03-21 12:54:02 -0500

    Update callout colors

--- src/lib/cancelledVsFunded/CalloutSingle.svelte
+++ src/lib/cancelledVsFunded/CalloutSingle.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
   import NumberFlow from '@number-flow/svelte';
   import { type FormatParts } from '@/utils/format-money';
+  import { cn } from '../utils';
 
   let {
     parts,
   }: {
     parts: FormatParts | null;
   } = $props();
+
+  const colorClass = $derived.by(() => {
+    const isNegative = parts?.prefix.includes('-');
+    if (parts?.value) {
+      return isNegative ? 'text-(--red)' : 'text-(--middle-blue)';
+      // return parts.value > 0 ? 'text-(--orange)':'text-(--middle-green)'  ;
+    }
+
+    return 'text-muted-foreground';
+  });
 </script>
 
 <div class="callout">
   {#if parts}
-    <NumberFlow {...parts} class="text-lg font-bold md:text-2xl" />
+    <NumberFlow {...parts} class={cn('text-lg font-bold md:text-2xl', colorClass)} />
   {:else}
     <span class="text-muted-foreground text-lg font-bold">—</span>
   {/if}
@@ -21,11 +32,10 @@
   .callout {
     border-radius: 0.75rem;
     padding: 0 calc(2 * var(--spacing));
-    border: 2px solid var(--color-teal-300);
-    max-width: 8rem;
+    border: 1px solid var(--color-slate-300);
+    max-width: 9rem; /* Magic number to accomadate the widest value*/
     width: 100%;
     margin: 0 auto;
-    box-shadow: var(--tw-shadow);
     text-align: center;
 
     display: flex;
